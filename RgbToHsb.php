17fc354937464e892ed7e7a5f0db17251e755a3f
Guus <Guus@Guus-PC>
2013-11-15 00:05:04 +0100

    RgbToHsb uitgebreid met Hex to HSB converter

--- RgbToHsb.php
+++ RgbToHsb.php
@@ -6,6 +6,29 @@
  * and open the template in the editor.
  */
 
+/**
+ * Converts a 6-digit hex value (like #FFFFFF) philips Hue HSB value
+ * @param string $hex
+ * @return array an Array with three key-value pairs: ['H'] with value from 0 to 65536, ['S']: values 0 - 255 and ['B']: values 0 - 255
+ */
+function hex_to_hsb($hex)
+{
+    $hex = str_replace("#", "", $hex);
+    
+    $R = hexdec(substr($hex,0,2));
+    $G = hexdec(substr($hex,2,2));
+    $B = hexdec(substr($hex,4,2));
+    
+    return rgb_to_hsb($R, $G, $B);
+}
+
+/**
+ * Converts a 24-bit RGB value to a philips Hue HSB value
+ * @param integer $R Red value from 0 to 255
+ * @param integer $G Green value from 0 to 255
+ * @param integer $B Blue value from 0 to 255
+ * @return array an Array with three key-value pairs: ['H'] with value from 0 to 65536, ['S']: values 0 - 255 and ['B']: values 0 - 255
+ */
 function rgb_to_hsb($R, $G, $B)
 {
     $HSL = array();
@@ -60,9 +83,19 @@ function rgb_to_hsb($R, $G, $B)
     return $HSL;
 }
 
-$hsv = rgb_to_hsb(0, 0, 255);
+//Test
+//
+//$hsv = rgb_to_hsb(0, 0, 255);
+//
+//echo $hsv['H'] . "<br/>";
+//echo $hsv['S'] . "<br/>";
+//echo $hsv['V'] . "<br/>";
+//
+//$hsv = hex_to_hsb('#0000FF');
+//
+//echo $hsv['H'] . "<br/>";
+//echo $hsv['S'] . "<br/>";
+//echo $hsv['V'] . "<br/>";
 
-echo $hsv['H'] . "<br/>";
-echo $hsv['S'] . "<br/>";
-echo $hsv['V'] . "<br/>";
+
 ?>
